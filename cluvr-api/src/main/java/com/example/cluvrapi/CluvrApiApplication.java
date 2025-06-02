0349e5c3e3966070c0c3fc19cc4818d1a31a28d5
sinyoung0403 <tlstjs753@gmail.com>
2025-06-02 11:15:56 +0900

    Init/#1:init setting

--- cluvr-api/src/main/java/com/example/cluvrapi/CluvrApiApplication.java
+++ cluvr-api/src/main/java/com/example/cluvrapi/CluvrApiApplication.java
@@ -1,6 +1,5 @@
 package com.example.cluvrapi;
 
-import com.example.chat.service.ChatService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
