fe27930391a82b7b97f6917f8a4ce03d441ae08e
sinyoung0403 <tlstjs753@gmail.com>
2025-06-04 11:48:56 +0900

    Feat/#11:Til Repository - Custom Interface 분리 - QueryDsl 적용

---
+++ cluvr-api/src/main/java/com/example/cluvrapi/domain/til/repository/TilRepositoryImpl.java
@@ -0,0 +1,58 @@
+package com.example.cluvrapi.domain.til.repository;
+
+import static com.example.cluvrapi.domain.til.entity.QTil.til;
+
+import java.util.List;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+
+import com.example.cluvrapi.domain.common.dto.PageResponseDto;
+import com.example.cluvrapi.domain.til.dto.response.InfoTilResponseDto;
+import com.example.cluvrapi.domain.til.dto.response.QInfoTilResponseDto;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
+@RequiredArgsConstructor
+public class TilRepositoryImpl implements TilRepositoryCustom {
+
+	private final JPAQueryFactory jpaQueryFactory;
+
+	@Override
+	public InfoTilResponseDto findTilById(Long tilId) {
+		InfoTilResponseDto content = jpaQueryFactory
+			.select(new QInfoTilResponseDto(
+				til.user.id,    // User 고유 식별자
+				til.club.id,    // Club 고유 식별자
+				til.title,        // title 제목
+				til.content))    // content 내용
+			.from(til)
+			.where(til.id.eq(tilId))
+			.fetchOne();
+		return content;
+	}
+
+	@Override
+	public PageResponseDto<InfoTilResponseDto> findAllTilById(Long clubId, Pageable pageable) {
+		List<InfoTilResponseDto> content = jpaQueryFactory
+			.select(new QInfoTilResponseDto(
+				til.user.id,    // User 고유 식별자
+				til.club.id,    // Club 고유 식별자
+				til.title,        // title 제목
+				til.content))    // content 내용
+			.from(til)
+			.where(til.club.id.eq(clubId))
+			.offset(pageable.getOffset())
+			.limit(pageable.getPageSize())
+			.fetch();
+
+		Long total = jpaQueryFactory
+			.select(til.count())
+			.from(til)
+			.where(til.club.id.eq(clubId))
+			.fetchOne();
+
+		return PageResponseDto.toDto(new PageImpl<>(content, pageable, total));
+	}
+}
