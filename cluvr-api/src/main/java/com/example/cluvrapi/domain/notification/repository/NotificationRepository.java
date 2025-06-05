939ea3d54108278513a5d6af8c8b03dec2ba4713
escomputer <nujest307@naver.com>
2025-06-05 15:10:43 +0900

    Feat/#8:noti mvp rud (#24)
    * Feat/#8: 모듈 분리
    
    # Conflicts:
    #	settings.gradle
    
    * Feat/#8: 모듈 분리 -2
    
    * Feat/#8: rud 완성
    
    * Feat/#8: build.gradle 분리
    
    * Feat/#8: refactor

--- cluvr-api/src/main/java/com/example/cluvrapi/domain/notification/repository/NotificationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.cluvrapi.domain.notification.repository;
-
-import com.example.cluvrapi.domain.common.repository.BaseRepository;
-import com.example.cluvrapi.domain.notification.entity.Notification;
-
-public interface NotificationRepository extends BaseRepository<Notification,Long> {
-
-
-}
---
+++ cluvr-notifications/src/test/java/com/example/cluvrnotifications/CluvrNotificationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cluvrnotifications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CluvrNotificationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
