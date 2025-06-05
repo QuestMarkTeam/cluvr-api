6b1f5d38ae04883221de67d3b3a060cebcee9ded
sinyoung0403 <tlstjs753@gmail.com>
2025-06-05 10:12:23 +0900

    Feat/#17:ProblemForm Repository 구현 - QueryDSL 구현

---
+++ cluvr-api/src/main/java/com/example/cluvrapi/domain/applicationForm/repository/ProblemFormRepository.java
@@ -0,0 +1,9 @@
+package com.example.cluvrapi.domain.applicationForm.repository;
+
+import com.example.cluvrapi.domain.applicationForm.entity.ProblemForm;
+import com.example.cluvrapi.domain.common.repository.BaseRepository;
+
+public interface ProblemFormRepository
+	extends BaseRepository<ProblemForm, Long>, ProblemFormRepositoryCustom {
+
+}
