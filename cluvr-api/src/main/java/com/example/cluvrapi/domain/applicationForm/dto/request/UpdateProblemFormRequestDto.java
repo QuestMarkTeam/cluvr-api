dad6f7396659a20ba4b98ac7dc7defe92e08180d
sinyoung0403 <tlstjs753@gmail.com>
2025-06-05 10:31:24 +0900

    Feat/#17:Valid 추가 1. Entity 에 맞는 검증 추가

--- cluvr-api/src/main/java/com/example/cluvrapi/domain/applicationForm/dto/request/UpdateProblemFormRequestDto.java
+++ cluvr-api/src/main/java/com/example/cluvrapi/domain/applicationForm/dto/request/UpdateProblemFormRequestDto.java
@@ -1,15 +1,16 @@
 package com.example.cluvrapi.domain.applicationForm.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 import lombok.Getter;
 
 @Getter
 public class UpdateProblemFormRequestDto {
-	@NotBlank
+	@NotBlank(message = "문제 양식을 작성해주세요.")
 	private String problemTemplate;
-	@NotBlank
+	@NotBlank(message = "문제 설명을 작성해주세요.")
 	private String submissionInstructions;
-	@NotBlank
+	@Size(min = 2, max = 100, message = "체점 방식은 2자 이상 100자 이하로 작성해주세요.")
 	private String gradingCriteria;
 }
