934f3eb90e6ca141f99627e785afd66981223743
sinyoung0403 <tlstjs753@gmail.com>
2025-06-05 10:10:51 +0900

    Feat/#17:SubmissionForm Mvp 기능 구현

---
+++ cluvr-api/src/main/java/com/example/cluvrapi/domain/applicationForm/service/SubmissionFormService.java
@@ -0,0 +1,74 @@
+package com.example.cluvrapi.domain.applicationForm.service;
+
+import org.springframework.data.domain.Pageable;
+
+import com.example.cluvrapi.domain.applicationForm.dto.request.CreateSubmissionFormRequestDto;
+import com.example.cluvrapi.domain.applicationForm.dto.request.UpdateSubmissionTemplateRequestDto;
+import com.example.cluvrapi.domain.applicationForm.dto.response.CreateSubmissionFormResponseDto;
+import com.example.cluvrapi.domain.applicationForm.dto.response.InfoSubmissionFormResponseDto;
+import com.example.cluvrapi.domain.common.dto.PageResponseDto;
+import com.example.cluvrapi.global.exception.BusinessException;
+
+public interface SubmissionFormService {
+
+	/**
+	 * 설명: 클럽의 제출 양식을 생성하는 메서드
+	 *
+	 * @param clubId                   {설명: 클럽의 고유 식별자}
+	 * @param submissionFormRequestDto {설명: 가입 제출 양식에 필요한 정보}
+	 * @return CreateSubmissionFormResponseDto {설명: 클럽의 id 값 반환}
+	 * @throws BusinessException {404 NotFound}
+	 * @author sinyoung0403
+	 */
+
+	CreateSubmissionFormResponseDto createSubmissionForm(Long clubId,
+		CreateSubmissionFormRequestDto submissionFormRequestDto);
+
+	/**
+	 * 설명: 클럽의 제출양식을 단건조회하는 메서드
+	 *
+	 * @param clubId           {설명: 클럽 고유 식별자}
+	 * @param submissionFormId {설명: 제출 양식 고유 식별자}
+	 * @return InfoSubmissionFormResponseDto {제출 양식 정보}
+	 * @author sinyoung0403
+	 */
+
+	InfoSubmissionFormResponseDto findSubmissionFormById(Long clubId, Long submissionFormId);
+
+	/**
+	 * 설명: 클럽의 제출양식을 다건조회하는 메서드
+	 *
+	 * @param clubId   {설명: 첫 번째 매개변수 설명}
+	 * @param pageable {설명: 두 번째 매개변수 설명}
+	 * @return PageResponseDto<InfoSubmissionFormResponseDto> {제출 양식 정보 페이징 객체}
+	 * @author sinyoung0403
+	 */
+
+	PageResponseDto<InfoSubmissionFormResponseDto> findAllSubmissionForm(Long clubId, Pageable pageable);
+
+	/**
+	 * 설명: 클럽의 제출 양식을 수정하는 메서드
+	 *
+	 * @param clubId
+	 * @param submissionFormId                   {설명: ApplicationForm 의 고유 식별자}
+	 * @param updateSubmissionTemplateRequestDto {설명: 수정할 때 필요한 정보}
+	 * @throws BusinessException {404 NotFound}
+	 * @author sinyoung0403
+	 */
+
+	void updateSubmissionTemplate(Long clubId, Long submissionFormId,
+		UpdateSubmissionTemplateRequestDto updateSubmissionTemplateRequestDto);
+
+	/**
+	 * 설명: 클럽의 제출 양식을 삭제하는 메서드
+	 *
+	 * <p> 양식을 삭제 시 SoftDeleted 처리 및 Club 의 JoinType 이 가입 신청 양식으로 변경
+	 *
+	 * @param clubId           {설명: 클럽 의 고유 식별자}
+	 * @param submissionFormId {설명: SubmissionForm 의 고유 식별자}
+	 * @throws BusinessException {404 NotFound}
+	 * @author sinyoung0403
+	 */
+
+	void deleteSubmissionForm(Long clubId, Long submissionFormId);
+}
