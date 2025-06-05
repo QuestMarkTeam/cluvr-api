5b5ef00cc750fe58a9f76b02df71134adf5b2925
sinyoung0403 <tlstjs753@gmail.com>
2025-06-05 10:09:14 +0900

    Feat/#17:ProblemForm Dto 생성

---
+++ cluvr-api/src/main/java/com/example/cluvrapi/domain/applicationForm/dto/request/CreateProblemFormRequestDto.java
@@ -0,0 +1,15 @@
+package com.example.cluvrapi.domain.applicationForm.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+
+import lombok.Getter;
+
+@Getter
+public class CreateProblemFormRequestDto {
+	@NotBlank
+	private String problemTemplate;
+	@NotBlank
+	private String submissionInstructions;
+	@NotBlank
+	private String gradingCriteria;
+}
