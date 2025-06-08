14b6f73573c09ee3e20a26da8f523325c0d8631a
dnjs5024 <dnjs7469@naver.com>
2025-06-08 23:29:08 +0900

    Feat/#20: Rebasing
    # Conflicts:
    #	cluvr-api/src/main/java/com/example/cluvrapi/domain/clover/entity/Clover.java

---
+++ cluvr-api/src/main/java/com/example/cluvrapi/domain/analytics/repository/GradeStatRepository.java
@@ -0,0 +1,7 @@
+package com.example.cluvrapi.domain.analytics.repository;
+
+import com.example.cluvrapi.domain.analytics.entity.UserBoardStat;
+import com.example.cluvrapi.domain.common.repository.BaseRepository;
+
+public interface GradeStatRepository extends BaseRepository<UserBoardStat, Long> {
+}
