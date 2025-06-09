5b489bbb887ede3184a822aaf15ee4007406e416
sinyoung0403 <tlstjs753@gmail.com>
2025-06-09 15:10:33 +0900

    Doc/#27:주석 수정

--- cluvr-api/src/main/java/com/example/cluvrapi/domain/join/enums/FormFieldType.java
+++ cluvr-api/src/main/java/com/example/cluvrapi/domain/join/enums/FormFieldType.java
@@ -1,5 +1,9 @@
 package com.example.cluvrapi.domain.join.enums;
 
+/**
+ * 가입 신청 폼에서 개별 입력 필드의 용도를 정의하는 열거형입니다.
+ */
+
 public enum FormFieldType {
 	SUBMISSION, PROBLEM
 }
--- cluvr-api/src/main/java/com/example/cluvrapi/domain/join/enums/JoinStatus.java
+++ cluvr-api/src/main/java/com/example/cluvrapi/domain/join/enums/JoinStatus.java
@@ -1,5 +1,9 @@
 package com.example.cluvrapi.domain.join.enums;
 
+/**
+ * 가입 신청에서 가입 상태를 정의하는 열거형입니다.
+ */
+
 public enum JoinStatus {
 	PENDING,    // 요청됨 (아직 승인되지 않음)
 	APPROVED,   // 승인됨
