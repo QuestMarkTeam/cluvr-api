62dccc920782dc5f19d35ce6fb63631b90cb07a8
sinyoung0403 <tlstjs753@gmail.com>
2025-06-09 15:09:46 +0900

    Feat/#27:핵심 MVP 기능 구현

--- cluvr-api/src/main/java/com/example/cluvrapi/domain/join/controller/JoinController.java
+++ cluvr-api/src/main/java/com/example/cluvrapi/domain/join/controller/JoinController.java
@@ -4,17 +4,33 @@ import jakarta.validation.Valid;
 
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.cluvrapi.domain.common.annotation.Auth;
+import com.example.cluvrapi.domain.common.dto.AuthUser;
+import com.example.cluvrapi.domain.common.dto.PageResponseDto;
 import com.example.cluvrapi.domain.join.dto.request.CreateJoinRequestDto;
+import com.example.cluvrapi.domain.join.dto.request.UpdateJoinRequestDto;
 import com.example.cluvrapi.domain.join.dto.response.CreateJoinResponseDto;
+import com.example.cluvrapi.domain.join.dto.response.InfoJoinRequestResponseDto;
+import com.example.cluvrapi.domain.join.dto.response.MyClubJoinResponseDto;
+import com.example.cluvrapi.domain.join.dto.response.MyJoinRequestResponseDto;
 import com.example.cluvrapi.domain.join.service.JoinService;
 import com.example.cluvrapi.global.response.BaseResponse;
 import com.example.cluvrapi.global.response.ResponseCode;
+
+/**
+ * 가입 요청 관련 API 를 처리하는 컨트롤러입니다.
+ */
 
 @RestController
 @RequiredArgsConstructor
@@ -22,13 +38,119 @@ public class JoinController {
 
 	private final JoinService joinService;
 
+	/**
+	 * 설명: 특정 클럽에 가입 요청을 생성합니다.
+	 *
+	 * @param authUser       인증된 사용자 정보
+	 * @param clubId         가입 요청을 보낼 클럽의 ID
+	 * @param joinRequestDto 가입 요청에 필요한 정보
+	 * @return 생성된 가입 요청 정보
+	 * @author sinyoung0403
+	 */
+
 	@PostMapping("/clubs/{clubId}/join")
 	public ResponseEntity<BaseResponse<CreateJoinResponseDto>> createJoin(
-		// @Auth AuthUser authUser,
+		@Auth AuthUser authUser,
 		@PathVariable Long clubId,
 		@Valid @RequestBody CreateJoinRequestDto joinRequestDto
 	) {
-		CreateJoinResponseDto joinResponseDto = joinService.createJoin(1L, clubId, joinRequestDto);
+		CreateJoinResponseDto joinResponseDto = joinService.createJoin(authUser.id(), clubId, joinRequestDto);
 		return ResponseEntity.ok(BaseResponse.success(joinResponseDto, ResponseCode.CREATED));
+	}
+
+	/**
+	 * 설명: 특정 클럽에 도착한 모든 가입 요청을 페이징하여 조회합니다.
+	 *
+	 * @param clubId   클럽 ID
+	 * @param pageable 페이징 정보
+	 * @return 클럽에 대한 가입 요청 목록
+	 * @author sinyoung0403
+	 */
+
+	@GetMapping("/clubs/{clubId}/join")
+	public ResponseEntity<BaseResponse<PageResponseDto<MyClubJoinResponseDto>>> findAllJoinRequestByClubId(
+		@PathVariable Long clubId,
+		@PageableDefault Pageable pageable
+	) {
+		PageResponseDto<MyClubJoinResponseDto> joinResponseDto = joinService.findJoinRequestByClubId(clubId, pageable);
+		return ResponseEntity.ok(BaseResponse.success(joinResponseDto, ResponseCode.OK));
+	}
+
+	/**
+	 * 설명: 현재 로그인한 사용자가 보낸 모든 가입 요청을 페이징하여 조회합니다.
+	 *
+	 * @param authUser 인증된 사용자 정보
+	 * @param pageable 페이징 정보
+	 * @return 본인이 보낸 가입 요청 목록
+	 * @author sinyoung0403
+	 */
+
+	@GetMapping("/clubs/join/my")
+	public ResponseEntity<BaseResponse<PageResponseDto<MyJoinRequestResponseDto>>> findAllMyJoinRequests(
+		@Auth AuthUser authUser,
+		@PageableDefault Pageable pageable
+	) {
+		PageResponseDto<MyJoinRequestResponseDto> joinResponseDto = joinService.findMyJoinRequests(authUser.id(),
+			pageable);
+		return ResponseEntity.ok(BaseResponse.success(joinResponseDto, ResponseCode.OK));
+	}
+
+	/**
+	 * 설명: 클럽과 가입 요청 ID에 해당하는 가입 요청 상세 정보를 조회합니다.
+	 *
+	 * <p> 가입 양식(Answer)의 경우 응답이 없을 경우 해당 컬럼이 "null" 로 조회됩니다.
+	 *
+	 * @param clubId        클럽 ID
+	 * @param joinRequestId 가입 요청 ID
+	 * @return 가입 요청 상세 정보
+	 * @author sinyoung0403
+	 */
+
+	@GetMapping("/clubs/{clubId}/join/{joinRequestId}")
+	public ResponseEntity<BaseResponse<InfoJoinRequestResponseDto>> findJoinRequestById(
+		@PathVariable Long clubId,
+		@PathVariable Long joinRequestId
+	) {
+		InfoJoinRequestResponseDto joinResponseDto = joinService.findJoinRequestById(clubId,
+			joinRequestId);
+		return ResponseEntity.ok(BaseResponse.success(joinResponseDto, ResponseCode.OK));
+	}
+
+	/**
+	 * 설명: 클럽과 가입 요청 ID에 해당하는 가입 요청의 답변을 수정합니다.
+	 *
+	 * @param clubId               클럽 ID
+	 * @param joinRequestId        가입 요청 ID
+	 * @param updateJoinRequestDto 수정할 답변 정보
+	 * @return 처리 결과
+	 * @author sinyoung0403
+	 */
+
+	@PatchMapping("/clubs/{clubId}/join/{joinRequestId}")
+	public ResponseEntity<BaseResponse<Void>> updateJoinRequestAnswer(
+		@PathVariable Long clubId,
+		@PathVariable Long joinRequestId,
+		@RequestBody UpdateJoinRequestDto updateJoinRequestDto
+	) {
+		joinService.updateJoinRequestAnswer(clubId, joinRequestId, updateJoinRequestDto);
+		return ResponseEntity.ok(BaseResponse.success(ResponseCode.NO_CONTENT));
+	}
+
+	/**
+	 * 특정 클럽에 보낸 가입 요청을 취소합니다.
+	 *
+	 * @param clubId        클럽 ID
+	 * @param joinRequestId 가입 요청 ID
+	 * @return No content
+	 * @author sinyoung0403
+	 */
+
+	@DeleteMapping("/clubs/{clubId}/join/{joinRequestId}")
+	public ResponseEntity<BaseResponse<Void>> cancelJoinRequest(
+		@PathVariable Long clubId,
+		@PathVariable Long joinRequestId
+	) {
+		joinService.cancelJoinRequest(clubId, joinRequestId);
+		return ResponseEntity.ok(BaseResponse.success(ResponseCode.NO_CONTENT));
 	}
 }
--- cluvr-api/src/main/java/com/example/cluvrapi/domain/join/service/JoinService.java
+++ cluvr-api/src/main/java/com/example/cluvrapi/domain/join/service/JoinService.java
@@ -1,8 +1,99 @@
 package com.example.cluvrapi.domain.join.service;
 
+import org.springframework.data.domain.Pageable;
+
+import com.example.cluvrapi.domain.common.dto.PageResponseDto;
 import com.example.cluvrapi.domain.join.dto.request.CreateJoinRequestDto;
+import com.example.cluvrapi.domain.join.dto.request.UpdateJoinRequestDto;
 import com.example.cluvrapi.domain.join.dto.response.CreateJoinResponseDto;
+import com.example.cluvrapi.domain.join.dto.response.InfoJoinRequestResponseDto;
+import com.example.cluvrapi.domain.join.dto.response.MyClubJoinResponseDto;
+import com.example.cluvrapi.domain.join.dto.response.MyJoinRequestResponseDto;
+import com.example.cluvrapi.global.exception.BusinessException;
+
+/**
+ * 가입 요청(Join Request)에 대한 비즈니스 로직을 정의하는 서비스 인터페이스입니다.
+ */
 
 public interface JoinService {
+
+	/**
+	 * 설명: 가입 신청을 하는 메서드
+	 *
+	 * <p>{가입 양식 별로 가입 신청 처리}
+	 *
+	 * @param userId         {설명: 로그인한 유저의 고유 식별자}
+	 * @param clubId         {설명: 클럽 고유 식별자}
+	 * @param joinRequestDto {설명: Join Request 에 필요한 정보}
+	 * @return CreateJoinResponseDto {설명: Join Request ID 반환}
+	 * @throws BusinessException {404 NOT_FOUND}
+	 * @throws BusinessException {INVALID_REQUEST: 400 BAD_REQUEST}
+	 * @author sinyoung0403
+	 */
+
 	CreateJoinResponseDto createJoin(Long userId, Long clubId, CreateJoinRequestDto joinRequestDto);
+
+	/**
+	 * 설명: 클럽의 고유 식별자를 통해 Join Request 조회하는 메서드
+	 *
+	 * <p> 클럽 운영자를 기준으로 조회하는 메서드
+	 *
+	 * @param clubId   {설명: 클럽 고유 식별자}
+	 * @param pageable {설명: 페이징 객체}
+	 * @return PageResponseDto<MyClubJoinResponseDto> {Join Request 정보 페이징 객체 -  클랜 운영진 기준}
+	 * @author sinyoung0403
+	 */
+
+	PageResponseDto<MyClubJoinResponseDto> findJoinRequestByClubId(Long clubId, Pageable pageable);
+
+	/**
+	 * 설명: 유저의 고유 식별자를 통해 Join Request 조회하는 메서드
+	 *
+	 * <p> 클럽을 신청한 자를 기준으로 조회하는 메서드
+	 *
+	 * @param userId   {설명: 유저 고유 식별자}
+	 * @param pageable {설명: 페이징 객체}
+	 * @return PageResponseDto<MyJoinRequestResponseDto> {Join Request 정보 페이징 객체 - 가입 유저 기준}
+	 * @author sinyoung0403
+	 */
+
+	PageResponseDto<MyJoinRequestResponseDto> findMyJoinRequests(Long userId, Pageable pageable);
+
+	/**
+	 * 설명: Join Request 의 고유 식별자를 통해 조회하는 메서드
+	 *
+	 * <p> 단일 Request 를 조회하는 메서드이다. 이때, Answer 또한 조회되며 존재하지 않을 시 "null"로 반환된다.
+	 *
+	 * @param clubId        {설명: 클럽 고유 식별자}
+	 * @param joinRequestId {설명: Join Request 고유 식별자}
+	 * @return 가입 요청 상세 정보
+	 * @author sinyoung0403
+	 */
+
+	InfoJoinRequestResponseDto findJoinRequestById(Long clubId, Long joinRequestId);
+
+	/**
+	 * 설명: Join Request 의 고유 식별자를 통해 조회하는 메서드
+	 *
+	 * @param clubId               {설명: 클럽 고유 식별자}
+	 * @param joinRequestId        {설명: Join Request 고유 식별자}
+	 * @param updateJoinRequestDto {설명: Join Request 에 필요한 정보 }
+	 * @throws BusinessException {ResponseCode.NOT_FOUND}
+	 * @author sinyoung0403
+	 */
+
+	void updateJoinRequestAnswer(Long clubId, Long joinRequestId, UpdateJoinRequestDto updateJoinRequestDto);
+
+	/**
+	 * 설명: Join Request Status 를 Cancel 상태로 변경해주는 메서드
+	 *
+	 * <p> Join Type 이 Form Type 일 경우, Cancel 과 동시에 Answer Data 를 Soft Deleted 처리한다.
+	 *
+	 * @param clubId        {설명: 클럽 고유 식별자}
+	 * @param joinRequestId {설명: Join Request 고유 식별자}
+	 * @throws BusinessException {ResponseCode.NOT_FOUND}
+	 * @author sinyoung0403
+	 */
+
+	void cancelJoinRequest(Long clubId, Long joinRequestId);
 }
