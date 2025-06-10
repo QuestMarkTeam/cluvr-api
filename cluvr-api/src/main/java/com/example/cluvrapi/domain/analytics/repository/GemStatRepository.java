af41f546756a0ca3c9f36a66f859575d8b4b5e24
dnjs5024 <dnjs7469@naver.com>
2025-06-10 12:28:26 +0900

    Feat/#20: 포인트 통계 삭제, localdatetime직렬화 추가

--- cluvr-api/src/main/java/com/example/cluvrapi/domain/analytics/repository/GemStatRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.cluvrapi.domain.analytics.repository;
-
-import com.example.cluvrapi.domain.analytics.entity.GemStat;
-import com.example.cluvrapi.domain.common.repository.BaseRepository;
-
-public interface GemStatRepository extends BaseRepository<GemStat, Long> {
-}
--- cluvr-api/src/main/java/com/example/cluvrapi/domain/analytics/service/GemStatService.java
+++
@@ -1,4 +0,0 @@
-package com.example.cluvrapi.domain.analytics.service;
-
-public interface GemStatService extends BusinessStatService {
-}
