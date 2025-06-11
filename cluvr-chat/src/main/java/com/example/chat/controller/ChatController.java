da71520178819440ffd1ff3604111f9449aedece
Tcimel <61877802+Tcimel@users.noreply.github.com>
2025-06-11 14:56:02 +0900

    Feat/#9 chat ws3 (#51)
    * Feat/#9: chat 기본 기능 구현(html 연동)
    
    * Feat/#9: chat mysql연동(채팅방 생성, 채팅 로그 저장 및 조회)
    
    * Feat/#9: chat mysql연동(채팅방 생성, 채팅 로그 저장 및 조회) - 누락된 파일 2차 업로드
    
    * Feat/#9: chat mysql, MongoDB 연동(html 작업)
    
    * Fix/#9: 주석 추가
    
    * Fix/#9: 머지 이슈 수정 작업
    
    * Fix/#9: 머지 이슈 수정 작업2
    
    * Fix/#9: 머지 이슈 수정 작업3

--- cluvr-chat/src/main/java/com/example/chat/controller/ChatController.java
+++ cluvr-chat/src/main/java/com/example/chat/controller/ChatController.java
@@ -1,4 +1,135 @@
 package com.example.chat.controller;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.example.chat.dto.request.ChatMessageRequestDto;
+import com.example.chat.dto.request.ChatRoomRequestDto;
+import com.example.chat.dto.request.CreateChatRoomRequestDto;
+import com.example.chat.dto.request.JoinRequestDto;
+import com.example.chat.dto.response.ChatRoomResponseDto;
+import com.example.chat.entity.ChatLog;
+import com.example.chat.entity.ChatRoomUser;
+import com.example.chat.enums.ClubRole;
+import com.example.chat.service.ChatService;
+import com.example.global.response.response.BaseResponse;
+import com.example.global.response.response.ResponseCode;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
 public class ChatController {
+
+	private final SimpMessagingTemplate messagingTemplate;
+	private final ChatService chatService;
+
+	@PostMapping("/club/{clubId}/chat/join")
+	public ResponseEntity<BaseResponse<Void>> joinChatRoom(
+		@PathVariable Long clubId,
+		@RequestBody JoinRequestDto request
+	) {
+		chatService.join(clubId, request);
+		return ResponseEntity.ok(BaseResponse.success(ResponseCode.OK));
+	}
+
+	// 추후 개선 진행 : 컨트롤러 계층에서 DTO 로 변환해 최소·안전 정보만 반환.
+	@GetMapping("/room/{roomId}/users")
+	public ResponseEntity<BaseResponse<List<ChatRoomUser>>> getUserInRoom(@PathVariable Long roomId) {
+		List<ChatRoomUser> users = chatService.getUserInRoom(roomId);
+		return ResponseEntity.ok(BaseResponse.success(users, ResponseCode.OK));
+	}
+
+	@MessageMapping("/message") // /pub/chat/message
+	public void sendMessage(@Payload ChatMessageRequestDto request) {
+		chatService.broadcastMessage(request); // 채팅 로그 몽고디비에 저장
+	}
+
+	/**
+	 * 설명: {채팅 메세지 불러오는 메서드}
+	 * <p>
+	 * [고도화 할 때 입장한 후 부터의 메세지 캐싱 필요]
+	 *
+	 * @param clubId
+	 * @param roomId
+	 * @return roomId에 해당하는 채팅 메세지
+	 * @author Tcimel
+	 */
+
+	@GetMapping("/club/{clubId}/chat/{roomId}")
+	public ResponseEntity<BaseResponse<List<ChatLog>>> getMessages(
+		@PathVariable Long clubId,
+		@PathVariable Long roomId
+	) {
+		List<ChatLog> messages = chatService.getMessages(roomId);
+		return ResponseEntity.ok(BaseResponse.success(messages, ResponseCode.OK));
+	}
+
+	/**
+	 * 설명: 채팅방 만들기
+	 * <p>
+	 * 생성할 때, 클럽 Id, 만든 user Id, 생성을 요청한 user의 클럽 role 필요
+	 * 유저는 클럽장, 매니저, MEMBER 역할이 있고 클럽장과 매니저 등급만 채팅방 생성 가능
+	 *
+	 * @param CreateChatRoomRequestDto {설명: 채팅방 생성에 필요한 dto}
+	 * @return {생성 완료 시 ResponseCode.CREATED 반환}
+	 * @throws {생성 권한이 없을 경우 권한이 없다는 안내 메세지 반환}
+	 * @author Tcimel
+	 */
+
+	@PostMapping("/chat/create")
+	public ResponseEntity<BaseResponse<Void>> createChatRoom(
+		@RequestBody CreateChatRoomRequestDto request) {
+		log.info("방 생성 요청: {}", request);
+		if (request.getRole()
+			== ClubRole.MEMBER) { // 외부 API지만 같은 회사내 다른 서버 API를 호출하는거임. 나중에 지정한 경로에서만 접속 가능하도록 WebConfig, SocketConfig 변경 필요
+			return ResponseEntity.ok(BaseResponse.error(ResponseCode.ACCESS_DENIED, "권한이 없습니다. 관리자에게 문의하세요."));
+		}
+		chatService.createChatRoom(request);
+		return ResponseEntity.ok(BaseResponse.success(ResponseCode.CREATED));
+	}
+
+	/**
+	 * 설명: {채팅방 조회}
+	 * 채팅방 조회 시 클럽원 역할에 따라 불러오는 리스트가 다름.
+	 * 채팅방 타입은 MANAGER, MEMBER 타입이 있는데
+	 * 클럽장과 매니저 역할은 모든 채팅방 리스트가 보이고
+	 * 유저에게는 MANAGER 타입의 채팅방이 보이면 안됨.
+	 * <p>
+	 * [고도화 때 접근하는 IP 설정 필요]
+	 * 방법 1 : IP 화이트리스트 설정 (필터 설정하기)
+	 * 방법 2 : X-INTERNAL_KEY 설정 추가 해당 키값이 없거나 같지 않을 경우 돌려보내기
+	 * 방법 3 : Spring Security 사용
+	 * <p>
+	 * 조회인데 Post를 한 이유
+	 * 외부에서 직접 URL 조작을 막고 싶은 경우, Body 안에 정보를 넣고 POST로 전달하는 방식이 일반적이라고 함.
+	 *
+	 * @param clubId              {설명: 채팅방 정보를 조회할 clubId 정보}
+	 * @param ChatRoomResponseDto {설명: 유저 Role에 해당하는 채팅방 리스트를 불러와야 하기 때문에, requestbody로 userId와 user의 role정보 전달 필요}
+	 * @return {clubId와 user의 role에 따른 채팅방 리스트 반환}
+	 * @author Tcimel
+	 */
+
+	@PostMapping("/club/{clubId}/chat/list")
+	public ResponseEntity<BaseResponse<List<ChatRoomResponseDto>>> getChatRooms(
+		@PathVariable Long clubId,
+		@RequestBody ChatRoomRequestDto request
+	) {
+		log.info("🥕🥕🥕 clubId = {}", clubId);
+		log.info("🥕🥕🥕 request = {}", request);
+		List<ChatRoomResponseDto> chatRooms = chatService.findChatRoomByClubAndRole(clubId, request);
+		return ResponseEntity.ok(BaseResponse.success(chatRooms, ResponseCode.OK));
+	}
+
 }
