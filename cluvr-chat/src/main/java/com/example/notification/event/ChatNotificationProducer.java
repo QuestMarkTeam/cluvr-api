80ace5c5f3ecaf1b171993b011df4f441875cb7a
escomputer <nujest307@naver.com>
2025-06-11 19:18:50 +0900

    Feat/#56 : 교환기명 상수명 정의한걸로 변경

--- cluvr-chat/src/main/java/com/example/notification/event/ChatNotificationProducer.java
+++ cluvr-chat/src/main/java/com/example/notification/event/ChatNotificationProducer.java
@@ -5,6 +5,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Component;
 
+import com.example.global.config.RabbitConfig;
+
 @Component
 @RequiredArgsConstructor
 public class ChatNotificationProducer {
@@ -12,7 +14,7 @@ public class ChatNotificationProducer {
 
 	public void send(ChatNotificationEvent dto) {
 		rabbitTemplate.convertAndSend(
-			"chat.notification",           // 교환기 이름
+			RabbitConfig.EXCHANGE_NAME,           // 교환기 이름
 			"user." + dto.getReceiverId(),     // 라우팅 키 - user별 큐
 			dto                                 // 메시지 본문
 		);
