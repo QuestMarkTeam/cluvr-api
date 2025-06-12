344b4280977cda14d65ea2cb619fb499b802743b
escomputer <nujest307@naver.com>
2025-06-12 15:41:29 +0900

    Refactor/#52 : import문 매핑 실수

--- cluvr-notifications/src/main/java/com/example/cluvrnotifications/domain/notification/entity/NotificationDocument.java
+++ cluvr-notifications/src/main/java/com/example/cluvrnotifications/domain/notification/entity/NotificationDocument.java
@@ -1,10 +1,9 @@
 package com.example.cluvrnotifications.domain.notification.entity;
 
-import jakarta.persistence.Id;
-
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.example.cluvrnotifications.domain.notification.enums.NotiTargetType;
