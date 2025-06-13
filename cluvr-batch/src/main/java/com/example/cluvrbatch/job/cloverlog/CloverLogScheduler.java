141311429afccfe0d81443dc03ca7d78ed25e86e
dnjs5024 <dnjs7469@naver.com>
2025-06-13 17:01:09 +0900

    Feat/#67: batch 세팅

--- cluvr-batch/src/main/java/com/example/cluvrbatch/job/cloverlog/CloverLogScheduler.java
+++ cluvr-batch/src/main/java/com/example/cluvrbatch/job/cloverlog/CloverLogScheduler.java
@@ -3,8 +3,6 @@ package com.example.cluvrbatch.job.cloverlog;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobParameters;
-import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -15,16 +13,12 @@ public class CloverLogScheduler {
 
 	private final JobLauncher jobLauncher;
 	private final Job cloverLogJob;
+	private final CloverJobService cloverJobService;
 
 	@Scheduled(cron = "0 0 3 * * *") // 매일 새벽 3시
 	public void runPointLogJob() {
 		try {
-			JobParameters params = new JobParametersBuilder()
-				.addLong("timestamp", System.currentTimeMillis()) // 동일 JobInstance 방지
-				.toJobParameters();
-
-			jobLauncher.run(cloverLogJob, params);
-
+			cloverJobService.runJob();
 		} catch (Exception e) {
 			// 로깅 or 슬랙 알림
 			throw new IllegalStateException("PointLogJob 실행 실패", e);
--- cluvr-batch/src/main/java/com/example/cluvrbatch/job/gemlog/GemLogScheduler.java
+++ cluvr-batch/src/main/java/com/example/cluvrbatch/job/gemlog/GemLogScheduler.java
@@ -3,8 +3,6 @@ package com.example.cluvrbatch.job.gemlog;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobParameters;
-import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -14,17 +12,13 @@ import org.springframework.stereotype.Component;
 public class GemLogScheduler {
 
 	private final JobLauncher jobLauncher;
+	private final GemJobService gemJobService;
 	private final Job gemLogJob;
 
 	@Scheduled(cron = "0 0 3 * * *") // 매일 새벽 3시
 	public void runPointLogJob() {
 		try {
-			JobParameters params = new JobParametersBuilder()
-				.addLong("timestamp", System.currentTimeMillis()) // 동일 JobInstance 방지
-				.toJobParameters();
-
-			jobLauncher.run(gemLogJob, params);
-
+			gemJobService.runJob();
 		} catch (Exception e) {
 			// 로깅 or 슬랙 알림
 			throw new IllegalStateException("PointLogJob 실행 실패", e);
