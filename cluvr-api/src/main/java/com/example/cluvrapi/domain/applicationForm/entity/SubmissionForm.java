9df6459a919b82be3d22c41aae9ff3bdcb463f02
sinyoung0403 <tlstjs753@gmail.com>
2025-06-15 21:15:40 +0900

    Feat/#86:연관관계 수정

--- cluvr-api/src/main/java/com/example/cluvrapi/domain/applicationForm/entity/SubmissionForm.java
+++ cluvr-api/src/main/java/com/example/cluvrapi/domain/applicationForm/entity/SubmissionForm.java
@@ -7,14 +7,12 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import org.hibernate.annotations.SQLDelete;
 
 import com.example.cluvrapi.domain.club.entity.Club;
 import com.example.cluvrapi.domain.common.entity.BaseTimeEntity;
@@ -22,7 +20,6 @@ import com.example.cluvrapi.domain.common.entity.BaseTimeEntity;
 @Entity
 @Getter
 @Table(name = "submission_forms")
-@SQLDelete(sql = "UPDATE submission_forms SET is_deleted = true WHERE id = ?")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class SubmissionForm extends BaseTimeEntity {
 
@@ -39,15 +36,9 @@ public class SubmissionForm extends BaseTimeEntity {
 	/**
 	 * 클럽
 	 */
-	@ManyToOne(fetch = FetchType.LAZY)
+	@OneToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "club_id", nullable = false)
 	private Club club;
-
-	/**
-	 * Soft Deleted
-	 */
-	@Column(nullable = false)
-	private Boolean isDeleted = false;
 
 	/**
 	 * 생성자 메서드
