bb340b54cdf251fdf8a8f38a47f4c914139abc94
YongLeeCode <yonghwan311@gmail.com>
2025-06-24 20:08:08 +0900

    Feat/#135: 보드 서비스 리팩토링

--- src/main/java/com/example/cluvrapi/domain/board/service/BoardServiceImpl.java
+++ src/main/java/com/example/cluvrapi/domain/board/service/BoardServiceImpl.java
@@ -10,8 +10,8 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.example.cluvrapi.domain.board.dto.request.CreateBoardRequestDto;
 import com.example.cluvrapi.domain.board.dto.request.UpdateBoardRequestDto;
+import com.example.cluvrapi.domain.board.dto.response.ReadAllBoardsResponseDto;
 import com.example.cluvrapi.domain.board.dto.response.ReadBoardResponseDto;
-import com.example.cluvrapi.domain.board.dto.response.ReadBoardsResponseDto;
 import com.example.cluvrapi.domain.board.dto.response.ReadMyBoardsResponseDto;
 import com.example.cluvrapi.domain.board.entity.Board;
 import com.example.cluvrapi.domain.board.repository.BoardRepository;
@@ -26,6 +26,7 @@ import com.example.cluvrapi.domain.reply.entity.Reply;
 import com.example.cluvrapi.domain.reply.repository.ReplyRepository;
 import com.example.cluvrapi.domain.user.entity.User;
 import com.example.cluvrapi.domain.user.repository.UserRepository;
+import com.example.cluvrapi.global.exception.BusinessException;
 import com.example.cluvrapi.global.exception.NoPermissionException;
 import com.example.cluvrapi.global.response.ResponseCode;
 
@@ -39,35 +40,39 @@ public class BoardServiceImpl implements BoardService {
 	private final ReactionRepository reactionRepository;
 	private final ReplyRepository replyRepository;
 	private final CloverRepository cloverRepository;
+	private final BoardViewCountRedisService boardViewCountRedisService;
 
 	@Override
 	@Transactional
 	public long createBoard(long userId, CreateBoardRequestDto dto) {
 		User user = userRepository.findByIdOrElseThrow(userId);
-		// Clover clover = cloverRepository.findByUserId(userId)
-		// 	.orElseThrow(() -> new IllegalStateException(ResponseCode.INVALID_REQUEST.getDefaultMessage()));
-		//
-		// clover.spendScore(dto.getClover());
+		Clover clover = cloverRepository.findByUserId(userId)
+			.orElseThrow(() -> new IllegalStateException(ResponseCode.INVALID_REQUEST.getDefaultMessage()));
+
+		clover.spendScore(dto.getClover());
 		return boardRepository.save(dto.fromDto(user)).getId();
 	}
 
 	@Override
 	@Transactional(readOnly = true)
-	public PageResponseDto<ReadBoardsResponseDto> readBoards(CategoryType category, Pageable pageable) {
+	public PageResponseDto<ReadAllBoardsResponseDto> readBoards(CategoryType category, Pageable pageable) {
 		return boardRepository.findAllBoardsByCategory(category, pageable);
 	}
 
 	@Override
 	@Transactional
-	public ReadBoardResponseDto readBoard(long boardId) {
-		boardRepository.incrementViewCount(boardId);
+	public ReadBoardResponseDto readBoard(long boardId, long userId) {
+		//고도화 예정
 		Board board = boardRepository.findBoardById(boardId);
 		Map<ReactionType, Long> reactionCountMap = reactionRepository.countBoardReactions(board);
 
+		// 좋아요 싫어요 수도 고도화 필요
 		long likeCount = reactionCountMap.getOrDefault(ReactionType.LIKE, 0L);
 		long dislikeCount = reactionCountMap.getOrDefault(ReactionType.DISLIKE, 0L);
 
-		return ReadBoardResponseDto.ofDto(board, likeCount, dislikeCount);
+		long viewCount = boardViewCountRedisService.incrementViewCount(board, userId);
+
+		return ReadBoardResponseDto.ofDto(board, viewCount, likeCount, dislikeCount);
 	}
 
 	@Transactional
@@ -75,18 +80,25 @@ public class BoardServiceImpl implements BoardService {
 	public void updateBoard(long userId, UpdateBoardRequestDto dto, long boardId) {
 		User user = userRepository.findByIdOrElseThrow(userId);
 		Board board = boardRepository.findByIdOrElseThrow(boardId);
+		int clover = board.getClover();
 
+		if (dto.getClover() != null) {
+			clover += dto.getClover();
+		}
+
+		// 게시글을 작성한 유저인지 확인
 		if (!board.getUser().equals(user)) {
 			throw new NoPermissionException(ResponseCode.NO_PERMISSION_DELETE,
 				ResponseCode.NO_PERMISSION_DELETE.getDefaultMessage());
 		}
 
-		if (board.getClover() + dto.getClover() > 110) {
-			throw new IllegalArgumentException("클로버는 110 아래로만 걸 수 있습니다.");
+		// 클로버 합산 조건 확인 (dto.getClover()가 null일 수도 있음)
+		if (dto.getClover() != null && clover > 100) {
+			throw new BusinessException(ResponseCode.CLOVER_POST_LIMIT);
 		}
 
-		board.update(dto.getTitle(), dto.getContent(), dto.getClover());
-		// boardRepository.updateBoard(dto.getTitle(), dto.getContent(), dto.getClover());
+		boardRepository.updateBoard(boardId, dto.getTitle(), dto.getContent(), clover);
+		System.out.println("test");
 	}
 
 	@Transactional
@@ -118,7 +130,8 @@ public class BoardServiceImpl implements BoardService {
 
 		// 보드의 isSelected가 true일 경우 사용 불가능
 		if (board.isSelected()) {
-			throw new NoPermissionException(ResponseCode.ACCESS_DENIED, ResponseCode.ACCESS_DENIED.getDefaultMessage());
+			throw new BusinessException(ResponseCode.ALREADY_SELECTED);
+
 		}
 		// 게시글을 작성한 유저가 아닐 경우 채택 불가능
 		if (!user.equals(board.getUser())) {
@@ -127,14 +140,14 @@ public class BoardServiceImpl implements BoardService {
 
 		// 게시글 유저와 댓글 유저가 동일하면 채택 불가능
 		if (board.getUser().equals(reply.getUser())) {
-			throw new NoPermissionException(ResponseCode.ACCESS_DENIED, ResponseCode.ACCESS_DENIED.getDefaultMessage());
+			throw new BusinessException(ResponseCode.CANNOT_SELECT_OWN_REPLY);
 		}
 
 		// 댓글이 게시글 소속의 댓글이 아니면 채택 불가능
 		if (!board.equals(reply.getBoard())) {
-			throw new NoPermissionException(ResponseCode.ACCESS_DENIED, ResponseCode.ACCESS_DENIED.getDefaultMessage());
+			throw new BusinessException(ResponseCode.REPLY_NOT_MATCHED_WITH_BOARD);
 		}
-		//
+
 		// 게시글에 있던 클로버를 댓글의 주인에게 클로버를 지급한다.
 		clover.takeScore(board.getClover());
 
