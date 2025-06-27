e82d463e22de92f793a1f396508dca9370263827
dnjs5024 <dnjs7469@naver.com>
2025-06-27 18:55:39 +0900

    feat/#155 결제 흐름 70퍼 구현
    # Conflicts:
    #	src/main/java/com/example/cluvrapi/domain/payment/controller/PaymentController.java
    #	src/main/java/com/example/cluvrapi/domain/payment/controller/PaymentViewController.java
    #	src/main/java/com/example/cluvrapi/domain/payment/service/PaymentService.java
    #	src/main/java/com/example/cluvrapi/domain/payment/service/PaymentServiceImpl.java
    #	src/main/resources/static/index.html
    #	src/main/resources/static/js/checkout.js

--- src/main/java/com/example/cluvrapi/domain/payment/service/PaymentServiceImpl.java
+++ src/main/java/com/example/cluvrapi/domain/payment/service/PaymentServiceImpl.java
@@ -2,6 +2,7 @@ package com.example.cluvrapi.domain.payment.service;
 
 import lombok.extern.slf4j.Slf4j;
 
+import org.apache.juli.logging.Log;
 import org.springframework.stereotype.Service;
 
 import com.example.cluvrapi.domain.payment.dto.request.PaymentPrepareRequestDto;
