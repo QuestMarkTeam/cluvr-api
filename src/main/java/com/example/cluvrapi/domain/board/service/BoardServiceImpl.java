a061c371c994d697488184dc3ade688fc5ba25bd
dnjs5024 <dnjs7469@naver.com>
2025-06-27 11:07:43 +0900

    feat/#155 yml 오류 수정

--- src/main/java/com/example/cluvrapi/domain/board/service/BoardServiceImpl.java
+++ src/main/java/com/example/cluvrapi/domain/board/service/BoardServiceImpl.java
@@ -30,8 +30,6 @@ import com.example.cluvrapi.domain.user.entity.User;
 import com.example.cluvrapi.domain.user.repository.UserRepository;
 import com.example.cluvrapi.global.annotation.UpdateClover;
 import com.example.cluvrapi.global.exception.BusinessException;
-import com.example.cluvrapi.global.annotation.EarnClover;
-import com.example.cluvrapi.global.annotation.EarnGem;
 import com.example.cluvrapi.global.annotation.EventGem;
 
 import com.example.cluvrapi.global.exception.NoPermissionException;
@@ -49,9 +47,7 @@ public class BoardServiceImpl implements BoardService {
 	private final CloverRepository cloverRepository;
 	private final BoardViewCountRedisService boardViewCountRedisService;
 
-	@EarnClover(value = CloverUserActivityType.CREATE_QUESTION)
 	@EventGem(value = GemUserActivityType.BOARD)
-	@UpdateClover(value = CloverUserActivityType.CREATE_QUESTION)
 	@UpdateClover(value = CloverUserActivityType.CREATE_QUESTION)
 	@Override
 	@Transactional
