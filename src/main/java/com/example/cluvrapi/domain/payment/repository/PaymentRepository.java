f7880e144d0cd13b09ec8fbe70408f2339346a0d
dnjs5024 <dnjs7469@naver.com>
2025-07-01 02:45:07 +0900

    feat/#155 결제시스템 구현 완료

--- src/main/java/com/example/cluvrapi/domain/payment/repository/PaymentRepository.java
+++ src/main/java/com/example/cluvrapi/domain/payment/repository/PaymentRepository.java
@@ -1,17 +1,10 @@
 package com.example.cluvrapi.domain.payment.repository;
 
-import java.util.Optional;
 
 import com.example.cluvrapi.domain.common.repository.BaseRepository;
-import com.example.cluvrapi.domain.payment.dto.response.FindPaymentPrepareResponseDto;
-import com.example.cluvrapi.domain.payment.entity.PaymentPending;
-import com.example.cluvrapi.global.exception.BusinessException;
-import com.example.cluvrapi.global.response.ResponseCode;
+import com.example.cluvrapi.domain.payment.entity.Payment;
 
-public interface PaymentRepository extends BaseRepository<PaymentPending,Long>,PaymentRepositoryQuery  {
-	public Optional<PaymentPending> findPaymentPendingByOrderId(String orderId);
 
-	default public PaymentPending findPaymentPrepareByOrderIdOrElseThrow(String orderId){
-		return findPaymentPendingByOrderId(orderId).orElseThrow(()->new BusinessException(ResponseCode.PAYMENT_PREPARE_NOT_FOUND));
-	}
+public interface PaymentRepository extends BaseRepository<Payment, Long > {
+
 }
