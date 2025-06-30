4a9408d88be47a7b3a43468db18475fe901cd199
dnjs5024 <dnjs7469@naver.com>
2025-07-01 03:15:48 +0900

    feat/#155 결제, Gem 충전 연동

---
+++ src/main/java/com/example/cluvrapi/domain/gem/handler/GemChargeHandler.java
@@ -0,0 +1,27 @@
+package com.example.cluvrapi.domain.gem.handler;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.stereotype.Component;
+
+import com.example.cluvrapi.domain.gem.dto.request.UpdateGemRequestDto;
+import com.example.cluvrapi.domain.gem.enums.GemActionType;
+import com.example.cluvrapi.domain.gem.service.GemService;
+
+@Component
+@RequiredArgsConstructor
+public class GemChargeHandler implements GemMethodHandler { // 이벤트 적립 처리
+
+	private final GemService gemService; // 내부에서 트랜잭션 공유 가능하도록
+
+	@Override
+	public boolean supports(GemActionType type) {
+		return GemActionType.CHARGE == type;
+	}
+
+	@Override
+	public void handle(Long userId, UpdateGemRequestDto dto) {
+		gemService.chargeGem(userId, dto);
+	}
+
+}
--- src/main/java/com/example/cluvrapi/domain/payment/dto/request/PaymentConfirmRequestDto.java
+++ src/main/java/com/example/cluvrapi/domain/payment/dto/request/PaymentConfirmRequestDto.java
@@ -2,9 +2,12 @@ package com.example.cluvrapi.domain.payment.dto.request;
 
 import lombok.Getter;
 
+import com.example.cluvrapi.domain.gem.dto.GemUpdateDto;
+
 @Getter
-public class PaymentConfirmRequestDto {
+public class PaymentConfirmRequestDto implements GemUpdateDto {
 	private String paymentKey;
 	private String orderId;
-	private Integer amount;
+	private Integer gem;
+
 }
