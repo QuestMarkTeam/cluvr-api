6eebada59f9576becd8638046ceeac03e7e633a3
sinyoung0403 <tlstjs753@gmail.com>
2025-07-01 18:09:29 +0900

    Feat/#187:검증 로직 별도 분리

---
+++ src/main/java/com/example/cluvrapi/domain/join/validator/ClubJoinValidator.java
@@ -0,0 +1,61 @@
+package com.example.cluvrapi.domain.join.validator;
+
+import java.util.Optional;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.stereotype.Service;
+
+import com.example.cluvrapi.domain.club.entity.Club;
+import com.example.cluvrapi.domain.clubMember.entity.enums.ClubMemberStatus;
+import com.example.cluvrapi.domain.clubMember.repository.ClubMemberRepository;
+import com.example.cluvrapi.domain.join.entity.JoinRequest;
+import com.example.cluvrapi.domain.join.enums.JoinStatus;
+import com.example.cluvrapi.domain.join.repository.JoinRequestRepository;
+import com.example.cluvrapi.domain.user.entity.User;
+import com.example.cluvrapi.global.exception.BusinessException;
+import com.example.cluvrapi.global.response.ResponseCode;
+
+@Service
+@RequiredArgsConstructor
+public class ClubJoinValidator {
+
+	private final JoinRequestRepository joinRequestRepository;
+	private final ClubMemberRepository clubMemberRepository;
+
+	public void validateJoinRequest(Club club, User user) {
+		validateNoDuplicateJoinRequest(club, user);
+		validateNotAlreadyMember(club, user);
+		validateNotFullMemberCount(club);
+	}
+
+	private void validateNoDuplicateJoinRequest(Club club, User user) {
+		Optional<JoinRequest> alreadyRequested = joinRequestRepository.findJoinByClubIdAndUserId(club.getId(),
+			user.getId());
+		alreadyRequested.ifPresent(joinRequest -> {
+			if (joinRequest.getJoinStatus() == JoinStatus.PENDING
+				|| joinRequest.getJoinStatus() == JoinStatus.APPROVED) {
+				throw new BusinessException(ResponseCode.INVALID_REQUEST, "이미 가입 신청한 클럽입니다.");
+			}
+		});
+	}
+
+	private void validateNotAlreadyMember(Club club, User user) {
+		clubMemberRepository.findByClubAndUserWithAnyStatus(club, user).ifPresent(clubMember -> {
+			switch (clubMember.getClubMemberStatus()) {
+				case ACTIVE:
+					throw new BusinessException(ResponseCode.INVALID_REQUEST, "이미 가입 신청한 클럽입니다.");
+				case KICKED:
+					throw new BusinessException(ResponseCode.INVALID_REQUEST, "강퇴당한 클럽은 가입 불가합니다.");
+			}
+		});
+	}
+
+	public void validateNotFullMemberCount(Club club) {
+		if (clubMemberRepository.countByClubIdAndStatus(club.getId(), ClubMemberStatus.ACTIVE)
+			>= club.getMaxMemberCount()) {
+			throw new BusinessException(ResponseCode.INVALID_REQUEST, "이미 회원이 꽉 찼습니다.");
+		}
+	}
+}
+
