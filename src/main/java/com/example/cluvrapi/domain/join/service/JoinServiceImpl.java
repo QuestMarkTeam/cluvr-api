504704bcb13c90c4875b049a66f65c61a9e526c1
sinyoung0403 <tlstjs753@gmail.com>
2025-07-02 20:58:44 +0900

    Refactor/#187:알림메시지 개선

--- src/main/java/com/example/cluvrapi/domain/join/service/JoinServiceImpl.java
+++ src/main/java/com/example/cluvrapi/domain/join/service/JoinServiceImpl.java
@@ -144,7 +144,7 @@ public class JoinServiceImpl implements JoinService {
 		Long ownerUserId = clubOwner.getUser().getId();
 
 		if (!ownerUserId.equals(findUser.getId())) { // 자기 자신이 아닌 경우에만
-			String content = String.format("'%s'님이 '%s' 클럽에 가입 신청을 했습니다.", findUser.getName(), findClub.getName());
+			String content = String.format("'%s'님이 '%s' 클럽에 가입했습니다.", findUser.getName(), findClub.getName());
 
 			NotificationEvent event = NotificationEvent.from(ownerUserId,                 // 클럽장에게
 				NotificationType.JOIN_REQUEST, content, NotiTargetType.CLUB, clubId);
